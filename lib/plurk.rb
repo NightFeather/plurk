89d81d0143cf273240a4b73a88b599fc8f6b84b3
NightFeather <ss900328@gmail.com>
2016-11-16 02:27:30 +0800

    Simple gem setup

--- lib/plurk.rb
+++ lib/plurk.rb
@@ -1,5 +1,14 @@
-require "plurk/version"
+require 'plurk/version'
+require 'oauth'
+require 'json'
+require 'yaml'
+
+require_relative './plurk/build_class'
+require_relative './plurk/client'
 
 module Plurk
-  # Your code goes here...
+end
+
+Dir[File.join(__dir__,'/plurk/fixtures/*.yml')].each do |f|
+  Plurk::BuildClass.insert(Plurk, f)
 end
